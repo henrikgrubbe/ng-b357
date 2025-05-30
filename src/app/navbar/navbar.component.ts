b6c4f3c07116cfbb161afdf22f08a46bd914709b
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-05-30 17:45:00 +0200

    Update NG20

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, inject, DOCUMENT } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
